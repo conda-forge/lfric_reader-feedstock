fc0dca2b4a3446dd0d19d292a2f8c76960e7fda4
Wolfgang Hayek <wolfgang.hayek@niwa.co.nz>
2022-01-11 14:38:28 +1300

    Fix CMake failure with Py3.8 on Linux

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,8 @@ cmake ${CMAKE_ARGS} CMakeLists.txt \
       -DCMAKE_BUILD_TYPE=Release \
       -DBUILD_SHARED_LIBS=ON \
       -DBUILD_TESTING=OFF \
-      -DPython3_EXECUTABLE="$PYTHON"
+      -DPython3_EXECUTABLE="$PYTHON" \
+      -DOPENGL_opengl_LIBRARY=${BUILD_PREFIX}/${HOST}/sysroot/usr/lib64/libGL.so
 cmake --build . --target install --config Release
 
 # Copy the [de]activate scripts to $PREFIX/etc/conda/[de]activate.d.
